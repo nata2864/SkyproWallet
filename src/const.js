b672041c0a937c87c88da97c1b777bd804af2709
NataBash <2864tasha@gmail.com>
2025-04-24 18:22:39 +0200

    Добавляет АПИ для авторизации и эйндпойнты для адресов

--- src/const.js
+++ src/const.js
@@ -17,3 +17,18 @@ export const textSizes = {
     fontWeight: "700",
   },
 };
+
+
+// Апи для регистрации и авторизации пользователя
+export const API_URL = "https://wedev-api.sky.pro/api/user";
+
+
+// Eindpoints для адресной строки
+export const RoutesApp = {
+  MAIN: "/",
+  SIGN_IN: "/sign-in",
+  SIGN_UP: "/sign-up",
+  NOT_FOUND: "/*",
+  LOGIN: "/login",
+ 
+};
