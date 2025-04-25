86992385a907a38e62df5e0dffd0261a8d59d2a3
NataBash <2864tasha@gmail.com>
2025-04-25 18:11:53 +0200

    Создает кнопку выхода из приложения для проверки авторизации

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,27 @@
 import * as S from "./Main.styled";
+import { useContext
+ } from "react";
+ import { AuthContext } from "../../context/AuthContext";
+ import { useNavigate } from "react-router-dom";
+ import { RoutesApp } from "../../const";
+
 
 function Main() {
+  const navigate = useNavigate();
+  const {logout} = useContext(AuthContext);
+
+  function handleLogout(e) {
+    e.preventDefault();
+    logout();
+    navigate(RoutesApp.SIGN_IN);
+  }
   return (
+
+  
+
     <S.Main>
       <p>Это главная страница</p>
+      <button   onClick={handleLogout}>Выйти</button>
     </S.Main>
   );
 }
