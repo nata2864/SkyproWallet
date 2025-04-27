4e2e1dc83c2eb480be86498f8807324b1bd1a711
NataBash <2864tasha@gmail.com>
2025-04-27 15:36:27 +0200

    Создает и стилизует Инпут

---
+++ src/components/Input/Input.styled.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import { textSizes } from "../../const";
+
+export const BasisInput = styled.input`
+
+width: 100%;
+max-width: 400px;
+height: 39px;
+box-sizing: border-box;
+border-radius: 6px;
+gap: 12px;
+border: 0.5px solid;
+border-color:  #999999
+padding: 12px;
+
+  &::placeholder {
+    color: #999999; 
+    font-weight: ${textSizes.small.fontWeight};
+    font-size: ${textSizes.small.fontSize};
+    opacity: 1; 
+  }
+
+`;
