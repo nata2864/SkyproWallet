e03cb3394d174be93a1435f1295d6c97f035ddd7
NataBash <2864tasha@gmail.com>
2025-04-27 15:34:43 +0200

    Создает Лайот для отражения Хидер во всех окнах приложения

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -3,7 +3,7 @@ import MainPage from "./pages/MainPage/MainPage.jsx";
 import SignInPage from "./pages/SignInPage/SignInPage.jsx";
 import SignUpPage from "./pages/SignUpPage/SignUpPage .jsx";
 import { RoutesApp } from "./const.js";
-
+import Layout from "./components/Layout/Layout.jsx";
 import PrivateRoute from "./components/PrivateRoute.jsx";
 
 function AppRoutes() {
@@ -11,11 +11,13 @@ function AppRoutes() {
 
   return (
     <Routes>
-      <Route element={<PrivateRoute />}>  <Route path={RoutesApp.MAIN} element={<MainPage />} />
+    <Route element={<Layout />}>
+      <Route element={<PrivateRoute />}>
+        <Route path={RoutesApp.MAIN} element={<MainPage />} />
       </Route>
-  
-    <Route path={RoutesApp.SIGN_IN} element={<SignInPage  />} />
-    <Route path={RoutesApp.SIGN_UP} element={<SignUpPage />} />
+      <Route path={RoutesApp.SIGN_IN} element={<SignInPage />} />
+      <Route path={RoutesApp.SIGN_UP} element={<SignUpPage />} />
+    </Route>
   </Routes>
   );
 }
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,15 +1,9 @@
-import Header from "../../components/Header/Header";
 import Main from "../../components/Main/Main";
 
-import { Outlet } from "react-router-dom";
-
 function MainPage() {
-
   return (
     <div>
-      <Header />
       <Main />
-      < Outlet/>
     </div>
   );
 }
