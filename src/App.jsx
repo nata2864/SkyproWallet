2cf30b953bf8bc992d408089445362f42879aaa7
NataBash <2864tasha@gmail.com>
2025-04-27 15:32:51 +0200

    Удаляет ссылки на отсутствующие элементы, удаляет CSS файлы, наследуемые из коробки

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import "./App.css";
 import AppRoutes from "./AppRoutes";
 import { ToastContainer } from "react-toastify";
 import AuthProvider from "./context/AuthProvider";
+import { GlobalStyles } from "./components/GlobalStyles.styled";
 
 function App() {
   return (
     <AuthProvider>
+      <GlobalStyles />
       <AppRoutes />
-
       <ToastContainer autoClose={1500} />
     </AuthProvider>
   );
